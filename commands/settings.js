435421a12d8ae8d7b9aee2670139aca2e7e19524
NoScxped <acolesios@icloud.com>
2022-05-22 12:44:03 -0400

    bug fix

--- commands/settings.js
+++ commands/settings.js
@@ -1,17 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, Message, Discord, MessageEmbed } = require('discord.js');
-
+const { Permissions } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
     .setName('settings')
 	.setDescription('settings')
-    .addChannelOption(option => option.setName('poll').setDescription('Channel where polls go'))
-	.addChannelOption(option => option.setName('counting').setDescription('Channel for counting'))
-    .addChannelOption(option => option.setName('level-up').setDescription('Channel where level-ups are announced'))
+    .addChannelOption(option => option.setName('poll').setDescription('Polls channel (Requires Admin)'))
+	.addChannelOption(option => option.setName('counting').setDescription('Counting channel (Requires Admin)'))
+    .addChannelOption(option => option.setName('level-up').setDescription('Level-Up channel (Requires Admin)'))
     .toJSON(),
 
     async execute(interaction, data, client) {
         try {
+            if(interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR, Permissions.FLAGS.MANAGE_CHANNELS])){
         if(interaction.options.getChannel(`poll`)){
             data(`write`, `guild`, interaction.guild.id, `pollChannel`, interaction.options.getChannel(`poll`).id.toString())
             client.channels.cache.get(interaction.options.getChannel(`poll`).id.toString()).send(`This channel is now polls channel!`)
@@ -24,10 +25,15 @@ module.exports = {
 
             var counting = data(`read`, `guild`, interaction.guild.id, `countingChannel`).toString()
             data(`write`, `guild`, interaction.guild.id, `countingChannel`, interaction.options.getChannel(`counting`).id.toString())
-
+            try {
             client.channels.cache.get(counting).send(`This channel is no longer the counting channel, the new one is <#${interaction.options.getChannel(`counting`).id}>`)
+            } catch{
+                
+            }
             var val = parseInt(data(`read`, `guild`, interaction.guild.id, `countingNumber`)) + 1
             client.channels.cache.get(interaction.options.getChannel(`counting`).id.toString()).send(`This channel is now the counting channel! the next number is: **` + val + `**`)
+        } } else {
+            interaction.reply(`You do not have admin!`)
         }
         interaction.reply(`Settings updated!`)
     } catch(err) {
