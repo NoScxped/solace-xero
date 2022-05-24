7c882e189743073e8d888645d3bff07a41da1d5c
NoScxped <acolesios@icloud.com>
2022-05-24 19:59:57 -0400

    forgor to add inventory

--- commands/rank.js
+++ commands/rank.js
@@ -31,7 +31,7 @@ module.exports = {
                 .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL(), url: interaction.user.avatarURL() })
                 .setColor(`RANDOM`)
                 .setThumbnail(interaction.user.avatarURL())
-                .addField(`Credits`, data(`read`, `user`, interaction.user.id, `credits`), true)
+                .addField(`Credits (**C**)`, data(`read`, `user`, interaction.user.id, `credits`), true)
                 .addField(`Level`, data(`read`, `user`, interaction.user.id, `level`), true)
                 .addField(`XP`, data(`read`, `user`, interaction.user.id, `xp`), true)
                 .addField(`XP needed for level up`, data(`read`, `user`, interaction.user.id, `pointsNeeded`), true)
