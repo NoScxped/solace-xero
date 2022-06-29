4b8e0f8f307f56ed2353731235343895c31d3b1c
NoScxped <acolesios@icloud.com>
2022-06-29 12:50:52 -0700

    fixed faction delete

--- commands/faction.js
+++ commands/faction.js
@@ -21,6 +21,11 @@ module.exports = {
             .setName('invite')
             .setDescription('Invite a user to your faction')
             .addUserOption(option => option.setName('user').setDescription('User')))
+    .addSubcommand(subcommand =>
+        subcommand
+            .setName('info')
+            .setDescription('See faction info for a user')
+            .addUserOption(option => option.setName('factionuser').setDescription('Faction User')))
     .toJSON(),
 
     async execute(interaction, data, client, Discord, splashtext) {
@@ -52,9 +57,12 @@ module.exports = {
                 .addField('Level', '1', true)
                 .setFooter({ text: 'ID: ' + factionId})
                 .setColor('RANDOM')
+                interaction.channel.send('Factions are heavily Work-In-Progress, so be aware your faction may be deleted')
 
                 data('write', 'faction', factionId, 'name', factionName.toString())
                 data('write', 'faction', factionId, 'owner', interaction.user.id.toString())
+                data('write', 'faction', factionId, 'members', `${interaction.user.id.toString()}`)
+                data('write', 'faction', factionId, 'level', `1`)
                 data('write', 'user', interaction.user.id, 'faction', factionId.toString())
 
                 interaction.reply({embeds: [embed]})
@@ -75,13 +83,61 @@ module.exports = {
 
             if(data('read', 'faction', factionId, "owner") === interaction.user.id.toString()){
 
-                fs.unlinkSync(`./data/faction/${parseInt(data('read', 'user', interaction.user.id, 'faction'))}.json`)
-                data('write', 'user', interaction.user.id, 'faction', '0')
+                var dataArray = data('read', 'faction', factionId, 'members').split(' ')
 
+                dataArray.forEach(user => 
+
+                    data('write', 'user', user, "faction", '0')
+
+                )
+
+                fs.unlinkSync(`./data/faction/${factionId}.json`)
                 return interaction.reply('✔️ Faction Deleted ✔️')
 
             } else {
+
                 return interaction.reply('❌ You do not own this faction! ❌')
+
+            }
+
+        }
+
+        if(interaction.options.getSubcommand() === 'info'){
+
+            if(interaction.options.getUser(`factionuser`)){
+
+                interaction.user = interaction.options.getUser(`factionuser`)
+    
+                if(interaction.user.bot){
+    
+                    interaction.reply(`You cannot get information on a bot!`)
+                    return
+                    
+                }
+            }
+
+            var factionId = data('read', 'user', interaction.user.id, 'faction')
+
+            if(factionId != false || factionId != '0'){
+
+                var factionName = data('read', 'faction', factionId, 'name')
+                var factionOwner = data('read', 'faction', factionId, 'owner')
+                var factionMembers = data('read', 'faction', factionId, 'members').split(' ').length
+                var factionLevel = data('read', 'faction', factionId, 'level')
+
+                var embed = new MessageEmbed()
+                .setAuthor({name: '『 ' + factionName.toString() + ' 』'})
+                .addField(`Members`, factionMembers.toString(), true)
+                .addField(`Owner`, `<@!` + factionOwner + ">", true)
+                .addField(`Level`, factionLevel)
+                .setColor('RANDOM')
+                .setFooter({ text: splashtext, iconURL: client.user.avatarURL() });
+                await interaction.reply({embeds: [embed]})
+
+            } else {
+
+                interaction.reply('❌ This user is not in a faction! ❌')
+
             }
         }
     }
