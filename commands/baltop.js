2d1d6aa61376d440ba0f413d2a9b392bad971bd6
NoScxped <acolesios@icloud.com>
2022-07-05 20:43:09 -0700

    added balancetop

--- commands/baltop.js
+++ commands/baltop.js
@@ -1,18 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, Message, Discord, MessageEmbed } = require('discord.js');
-const fs = require('fs')
+const fs = require('fs');
+const { waitForDebugger } = require('inspector');
 const path = require('path')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('baltop')
 		.setDescription('Credits Balance Top'),
 	async execute(interaction, data, client, Discord, splashtext) {
+      const message = await interaction.reply({content: 'Xero is thinking...', fetchReply: true})
         var str = []
         var files = fs.readdirSync(path.resolve('./data/user'))
         for(const i of files){
             var credits = parseInt(data('read', 'user', i.slice(0, -5), 'credits'))
             var id = i.slice(0, -5)
-            if(!Number.isNaN(credits)){
+            if(!Number.isNaN(credits) && credits != false){
                 str.push({"credits": credits, "id": id})
             }
             
@@ -30,14 +32,14 @@ module.exports = {
         var num = 0
         for(var i in str){
             var name = ''
-            try {
-                name = client.users.cache.get(str[i].id).tag
-            }
-            catch {
-                name = `<@!${str[i].id}>`
-            }
-
             if(num <= 9){
+              try {
+                var user = await client.users.fetch(str[i].id.toString()).catch(console.error)
+                  name = "**" + user.username + "**#" + user.discriminator
+              }
+              catch {
+                  name = `<@!${str[i].id}>`
+              }
               num = num + 1
               msg = msg + `${num}. » *${name}* › ${str[i].credits} ⌬\n\n`
             }
@@ -48,7 +50,7 @@ module.exports = {
         .setColor("RANDOM")
         .setDescription(msg)
         .setFooter({ text: splashtext, iconURL: client.user.avatarURL() });
-        
-    await interaction.reply({embeds:[embed]})
+        message.edit({content: '_ _', embeds:[embed]})
+    
     }
 }
