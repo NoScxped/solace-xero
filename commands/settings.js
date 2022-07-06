97f307d0edc5769056e20c9949dd599f0d8f5023
NoScxped <acolesios@icloud.com>
2022-07-05 20:45:34 -0700

    added baltop, removed counting/stats

--- commands/settings.js
+++ commands/settings.js
@@ -7,8 +7,7 @@ module.exports = {
 	.setDescription('settings')
     .addChannelOption(option => option.setName('poll').setDescription('Polls channel (Requires Admin)'))
     .addBooleanOption(option => option.setName('levelup').setDescription('Set level up messages (Requires Admin)'))
-	.addChannelOption(option => option.setName('counting').setDescription('Counting channel (Requires Admin)'))
-    .addChannelOption(option => option.setName('gbcounting').setDescription('Global Counting channel (Requires Admin)'))
+	.addChannelOption(option => option.setName('counting').setDescription('Global Counting channel (Requires Admin)'))
     .toJSON(),
 
     async execute(interaction, data, client) {
@@ -21,12 +20,12 @@ module.exports = {
             client.channels.cache.get(interaction.options.getChannel(`poll`).id.toString()).send(`This channel is now polls channel!`)
 
         }
-        if(interaction.options.getChannel(`gbcounting`)){
+        if(interaction.options.getChannel(`counting`)){
             var arr = data(`read`, `global`, "gbcounting", `channels`).split(',')
             if(arr.includes(interaction.channel.id)){
                 return interaction.reply('This channel is already set!')
             }
-            arr.push(interaction.options.getChannel(`gbcounting`).id.toString())
+            arr.push(interaction.options.getChannel(`counting`).id.toString())
 
             data(`write`, `global`, "gbcounting", `channels`, arr.toString())
             try {
@@ -44,31 +43,7 @@ module.exports = {
                 data('write', 'guild', interaction.guild.id, 'levelMessages', 'false')
             }
 
-        if(interaction.options.getChannel(`counting`)){
-
-            var counting = ""
-
-            if(data(`read`, `guild`, interaction.guild.id, `countingChannel`) != false && data(`read`, `guild`, interaction.guild.id, `countingChannel`) != `NaN`){
-
-            counting = data(`read`, `guild`, interaction.guild.id, `countingChannel`).toString()
-
-            }
-
-            data(`write`, `guild`, interaction.guild.id, `countingChannel`, interaction.options.getChannel(`counting`).id.toString())
-            data(`write`, `guild`, interaction.guild.id, `countingNumber`, '0')
-
-            try {
-
-            client.channels.cache.get(counting).send(`This channel is no longer the counting channel, the new one is <#${interaction.options.getChannel(`counting`).id}>`)
-
-            } catch{
-                
-            }
-            var val = parseInt(data(`read`, `guild`, interaction.guild.id, `countingNumber`)) + 1
-
-            client.channels.cache.get(interaction.options.getChannel(`counting`).id.toString()).send(`This channel is now the ***Server* Counting Channel**! the next number is: **` + val + `**`)
-            interaction.channel.send('❗Be aware that the old version of counting will soon be removed in favor of GLobal Counting❗')
-        } } else {
+         } else {
 
             return interaction.reply(`You do not have admin!`)
 
