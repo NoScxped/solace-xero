5c9d134ba7166ce62411ab7c8b9686a1eea32e9b
NoScxped <acolesios@icloud.com>
2022-07-13 16:04:09 -0700

    fix

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const config = require('./config.json');
+const config = require('./configuration.json');
 const fs = require('node:fs');
 const apollo = require(`apollo.console`)
 
